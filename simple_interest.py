de58adb2a628be871e913780bc72cd7bbbd7ae69
saisuprithayerramsetty <saisuprithayerramsetty@gmail.com>
2026-09-07 21:07:40 +0530

    unit-1 task-6

---
+++ simple_interest.py
@@ -0,0 +1,5 @@
+principle = int(input("Enter the principle number:"))
+rate = int(input("Enter the rate of interest:"))
+time = int(input("Enter the time period:"))
+simple_interest = (principle*rate*time)/100
+print(simple_interest)
