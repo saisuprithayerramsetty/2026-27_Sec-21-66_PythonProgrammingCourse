29d81d7b58912c20ff743c761a09481185006894
saisuprithayerramsetty <saisuprithayerramsetty@gmail.com>
2026-09-11 14:29:31 +0530

    unit-1 task-10

---
+++ comparison opertors.py
@@ -0,0 +1,19 @@
+a = 2
+b = 5
+result = a==b
+print("result of",a,"==",b,"is:",result)
+
+result = a!=b
+print("result of",a,"!=",b,"is:",result)
+
+result = a<b
+print("result of",a,"<",b,"is:",result)
+
+result = a>b
+print("result of",a,">",b,"is:",result)
+
+result = a<=b
+print("result of",a,"<=",b,"is:",result)
+
+result = a>=b
+print("result of",a,">=",b,"is:",result)
