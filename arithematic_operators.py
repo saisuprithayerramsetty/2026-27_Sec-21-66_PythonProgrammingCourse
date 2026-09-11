bd6eb0b32c3dd75d80c983d85dc019b3b1f87d6f
saisuprithayerramsetty <saisuprithayerramsetty@gmail.com>
2026-09-11 14:27:25 +0530

    Add files via upload

---
+++ arithematic_operators.py
@@ -0,0 +1,22 @@
+a = 100
+b = 5
+result = a+b
+print("result of",a,"+",b,"is:",result)
+
+result = a-b
+print("result of",a,"-",b,"is:",result)
+
+result = a*b
+print("result of",a,"*",b,"is:",result)
+
+result = a/b
+print("result of",a,"/",b,"is:",result)
+
+result = a//b
+print("result of",a,"//",b,"is:",result)
+
+result = a%b
+print("result of",a,"%",b,"is:",result)
+
+result = a**b
+print("result of",a,"**",b,"is:",result)
