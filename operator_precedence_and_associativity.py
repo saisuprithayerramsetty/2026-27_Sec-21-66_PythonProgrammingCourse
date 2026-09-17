894debf267e02b9689a1d0ca58b223cee1f174da
saisuprithayerramsetty <saisuprithayerramsetty@gmail.com>
2026-09-17 22:16:34 +0530

    unit_1 task_14

---
+++ operator_precedence_and_associativity.py
@@ -0,0 +1,22 @@
+a = 2 + 3 * 4
+print("The value of a is:", a)
+
+
+b = (2 + 3) * 4
+print("The value of b is:", b)
+
+
+c = 2 + (3 * 4)
+print("The value of c is:", c)
+
+
+d = 2 ** 3 * 2
+print("The value of d is:", d)
+
+
+e = 2 / 3 // 4
+print("The value of e is:", e)
+
+
+f = 2 + 3 - 4 * 2 / 2 // 3 > 10
+print("The value of f is:", f)
