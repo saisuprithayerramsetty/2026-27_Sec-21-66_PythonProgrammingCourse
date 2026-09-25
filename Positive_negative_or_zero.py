7fe0a72bd0c2ff85746ec6a8a999b999ef25eb97
saisuprithayerramsetty <saisuprithayerramsetty@gmail.com>
2026-09-25 14:45:19 +0530

    task-3 unit-2

---
+++ Positive_negative_or_zero.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print(f"The number is:, positive (number = {number})")
+elif number == 0:
+    print(f"The number is:, zero (number = {number})")
+else:
+    print(f"The number is:, negative (number = {number})")
