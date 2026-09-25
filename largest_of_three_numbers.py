774864adcc1de3a8efeb41aa76b1c22667b0d093
saisuprithayerramsetty <saisuprithayerramsetty@gmail.com>
2026-09-25 15:01:07 +0530

    task-5 unit-2

---
+++ largest_of_three_numbers.py
@@ -0,0 +1,14 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+if a >= b and a >= c:
+    if a >= c:
+        largest = a
+    else:
+        largest = c
+else:
+    if b >= c:
+        largest = b
+    else:
+        largest = c
+print(f"Largest of three numbers = {largest} (among {a}, {b}, {c})")
