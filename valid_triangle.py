537b921e94b2053da68049de2358bc90af326cde
saisuprithayerramsetty <saisuprithayerramsetty@gmail.com>
2026-09-25 14:22:12 +0530

    task-1 unit-2

---
+++ valid_triangle.py
@@ -0,0 +1,7 @@
+a = int(input("enter a value:"))
+b = int(input("enter a value:"))
+c = int(input("enter a value:"))
+if a>0 and b>0 and c>0 and a+b+c==180:
+    print("valid")
+else:    
+    print("not valid")
