60140b056f0ff38d3f775e8cf03190ae3518542f
saisuprithayerramsetty <saisuprithayerramsetty@gmail.com>
2026-09-25 15:37:57 +0530

    task-1 unit-2

--- valid_triangle.py
+++ valid_triangle.py
@@ -1,7 +1,7 @@
-a = int(input("enter a value:"))
-b = int(input("enter a value:"))
-c = int(input("enter a value:"))
-if a>0 and b>0 and c>0 and a+b+c==180:
-    print("valid")
+angle_1_degrees = int(input("enter a first angle in degrees:"))
+angle_2_degrees = int(input("enter a second angle in degrees:"))
+angle_3_degrees = int(input("enter a third angle in degrees:"))
+if angle_1_degrees > 0 and angle_2_degrees > 0 and angle_3_degrees > 0 and angle_1_degrees+angle_2_degrees+angle_3_degrees == 180:
+    print(f"valid triangle, (sum of angles = {angle_1_degrees+angle_2_degrees+angle_3_degrees})")
 else:    
-    print("not valid")
+    print(f"not a valid triangle (sum of angles = {angle_1_degrees+angle_2_degrees+angle_3_degrees})")
